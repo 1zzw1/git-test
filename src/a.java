2b0c9ca9dccb72eb9b70bb0c076a6b8f728b4d3d
张志武 <10750066+zhang-zhiwu@user.noreply.gitee.com>
2024-05-19 11:47:02 +0000

    update src/a.java.
    Signed-off-by: 张志武 <10750066+zhang-zhiwu@user.noreply.gitee.com>

--- src/a.java
+++ src/a.java
@@ -2,6 +2,6 @@ public class a {
     public static void main(String[] args) {
         System.out.println("测试git");
         System.out.println("测试 commit");
-        System.out.println("测试pull");
+        System.out.println("测试pull1");
     }
 }
