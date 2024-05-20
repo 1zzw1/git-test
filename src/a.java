3d29f5daaf6a339fb6990cb9d24fc1133066d8b7
1zzw1 <1547231823@qq.com>
2024-05-20 14:38:19 +0800

    测试提交本地分支到远端

--- src/a.java
+++ src/a.java
@@ -3,5 +3,6 @@ public class a {
         System.out.println("测试git");
         System.out.println("测试 commit");
         System.out.println("测试pull");
+        System.out.println("测试将本地分支提交到远程分支");
     }
 }
